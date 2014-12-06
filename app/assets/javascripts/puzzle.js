238d6a7ab33877bf5665fc73cc7432b99d79d691
KaitoWatanabe <kalbus1892@gmail.com>
2014-12-07 02:17:10 +0900

    Add quiz initializer.

--- app/assets/javascripts/puzzle.js
+++ app/assets/javascripts/puzzle.js
@@ -1,9 +1,23 @@
 var complete, present;
-var correct = 0;
+var correct=0,clear=0;
+
+var starts = [
+  {"property":'font-size',"value":"20px"},
+  {"property":'border',"value":"1px double #000"},
+  {"property":'border-radius',"value":"0"}
+]
+
+var goals = [
+  {"property":'font-size',"value":"30px"},
+  {"property":'border',"value":"3px solid #f99"},
+  {"property":'border-radius',"value":"6px"}
+]
 
 $(function () {
   complete = $('#complete .element');
   present = $('#present .element');
+
+  initialize();
 
   $('#editor input').eq(0).focus();
 
@@ -24,21 +38,45 @@ $(function () {
       $(this).addClass('correct').find('label').before('<i class="fa fa-star">');
       $(this).next().find('input').focus();
       correct++;
-      if (correct === 5) {
+      console.log(correct);
+      if (correct == clear) {
         alert('CLEAR!');
       };
     }
     else{
       console.log('try again');
     }
-    
   });
 });
+
+function initialize () {
+  var frm = $('#editor form');
+  clear = starts.length;
+  console.log(starts.length);
+  for (var i = 0; i < starts.length; i++) {
+    present.css(starts[i].property, starts[i].value);
+    var id = "q"+i;
+    var label = $('<label>').attr('for', id).text(starts[i].property);
+    var input = $('<input>').attr({
+      type: 'text',
+      value: starts[i].value,
+      id: id,
+      name: id
+    });
+    var quiz = $('<div class="property">').append(label).append(input);
+    frm.append(quiz);
+    
+  }
+  for (var i = 0; i < goals.length; i++) {
+    complete.css(goals[i].property, goals[i].value);
+    
+  };
+}
 
 function check (property) {
   return complete.css(property) == present.css(property);
 }
 
-function alt () {
-  alert('右クリックさせないよ');
-}
+// function alt () {
+//   alert('右クリックさせないよ');
+// }
