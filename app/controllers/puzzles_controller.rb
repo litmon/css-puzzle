de8aeac21e9176aae379ef13f0d0ff55ad23a95b
Fukuo Kadota <app.fk91@gmail.com>
2014-12-08 21:53:58 +0900

    rename some files

--- app/controllers/puzzles_controller.rb
+++ app/controllers/puzzles_controller.rb
@@ -1,2 +1,35 @@
-class PuzzleController < ApplicationController
+class PuzzlesController < ApplicationController
+  
+  def index
+    @puzzles = Puzzle.all
+  end
+
+  def show
+    @puzzle = Puzzle.find(params[:id])
+  end
+
+  def new
+    @puzzle = Puzzle.new
+  end
+
+  def create
+    @puzzle = Puzzle.new(puzzle_params)
+  end
+
+  def delete
+    @puzzle = Puzzle.find(params[:id])
+
+    if @puzzle.delete
+
+    else
+
+    end
+  end
+
+  private
+
+  def puzzle_params
+    params.require(:puzzle).permit()
+  end
+
 end
