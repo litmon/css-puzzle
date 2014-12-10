905bbf811c44de31b7f27e465e9d0c2223875336
Fukuo Kadota <app.fk91@gmail.com>
2014-12-10 17:23:26 +0900

    Add model user

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
 
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations',
+    omniauth_callbacks: 'users/authentications'
+  }
+
   root 'puzzles#index'
 
   resources :puzzles
