d25d6ecc15f79b8c91d4bfc12963235fd28cce55
Fukuo Kadota <app.fk91@gmail.com>
2014-12-12 01:44:28 +0900

    Rewrite seeds

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141210030307) do
+ActiveRecord::Schema.define(version: 20141211161001) do
 
   create_table "authentications", force: true do |t|
     t.integer  "user_id"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20141210030307) do
     t.datetime "updated_at"
     t.string   "title"
     t.text     "description"
+    t.text     "html"
   end
 
   create_table "selectors", force: true do |t|
