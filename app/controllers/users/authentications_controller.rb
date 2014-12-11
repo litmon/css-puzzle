b82ed9ce34c862a93bd808f6534fbc234e0a42f4
Fukuo Kadota <app.fk91@gmail.com>
2014-12-12 03:03:09 +0900

    Add column icon(url) to user

--- app/controllers/users/authentications_controller.rb
+++ app/controllers/users/authentications_controller.rb
@@ -23,6 +23,8 @@ class Users::AuthenticationsController < Devise::OmniauthCallbacksController
   end
 
   def redirect_to_next
+    byebug
+
     if authentication
       sign_in(authentication.user)
       redirect_to root_path, notice: "User signed in."
@@ -31,6 +33,15 @@ class Users::AuthenticationsController < Devise::OmniauthCallbacksController
     else
       session[:omniauth] = omniauth
       redirect_to new_user_registration_path
+    end
+  end
+
+  def oauth_user_params
+    hash = request.env['omniauth.auth'][:info].slice(:name, :nickname, :image)
+    if hash.key?(:nickname)
+      { name: hash[:nickname], image: hash[:image] }
+    else
+      hash
     end
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141211161001) do
+ActiveRecord::Schema.define(version: 20141211180142) do
 
   create_table "authentications", force: true do |t|
     t.integer  "user_id"
@@ -52,7 +52,7 @@ ActiveRecord::Schema.define(version: 20141211161001) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
+    t.string   "email"
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -64,6 +64,8 @@ ActiveRecord::Schema.define(version: 20141211161001) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
+    t.string   "icon"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
